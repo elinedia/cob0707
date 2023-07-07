0c90941f77b14308d4e93f02f506568df9802c5e
kingian2023 <138881717+kingian2023@users.noreply.github.com>
2023-07-07 22:04:14 +0800

    Create 123.cs

---
+++ 123.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ca1
+{
+    internal class Program
+    {
+        struct Stu
+        {
+            public string stna;
+            public string stno;
+        }
+        // 結構設定
+        static void Main(string[] args)
+        {
+            Stu st1;
+            st1.stna = "李芃芃";
+            st1.stno = "sk00001";
+        }
+
+    }
+}
